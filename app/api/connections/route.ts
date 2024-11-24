1eafabde2cf142e2bd498657a30d32a978511331
Bob Remeika <bob@ragie.ai>
2024-11-24 13:33:27 -0800

    Order the connections by name

--- app/api/connections/route.ts
+++ app/api/connections/route.ts
@@ -2,6 +2,6 @@ import db from "@/lib/db";
 import * as schema from "@/lib/db/schema";
 
 export async function GET() {
-  const connections = await db.select().from(schema.connections);
+  const connections = await db.select().from(schema.connections).orderBy(schema.connections.name);
   return Response.json(connections);
 }
