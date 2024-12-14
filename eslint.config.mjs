23ccff509add5e324c12666921b359058d651334
Bob Remeika <bob@ragie.ai>
2024-12-13 17:56:00 -0800

    Upgrade eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import _import from "eslint-plugin-import";
+import react from "eslint-plugin-react";
+import { fixupPluginRules } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("next/core-web-vitals"), {
+    plugins: {
+        react
+    },
+
+    rules: {
+        "import/order": ["error", {
+            groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+            "newlines-between": "always",
+
+            alphabetize: {
+                order: "asc",
+                caseInsensitive: true,
+            },
+        }],
+
+        "react/no-unknown-property": ["error", {
+            ignore: ["css"],
+        }],
+    },
+}];
