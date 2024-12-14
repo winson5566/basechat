ea8684c488370ee0399e4770a325cec7bceb7e2c
Bob Remeika <bob@ragie.ai>
2024-12-13 17:56:55 -0800

    Docker build requires standalone output

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Set to false because strict mode breaks components that call APIs when the component is rendered (like in Conversation)
   reactStrictMode: false,
+  output: "standalone",
 };
 
 export default nextConfig;
