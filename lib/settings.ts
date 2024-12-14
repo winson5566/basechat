c52018d87715b8d35871e229600606ead6f751f1
Bob Remeika <bob@ragie.ai>
2024-12-13 17:56:28 -0800

    Remove assertions that cause the build to fail

--- lib/settings.ts
+++ lib/settings.ts
@@ -4,17 +4,17 @@ export const APP_NAME = "Base Chat";
 
 export const COMPANY_NAME = "Acme Corp";
 
-assert(process.env.BASE_URL);
-export const BASE_URL = process.env.BASE_URL;
+// assert(process.env.BASE_URL);
+export const BASE_URL = process.env.BASE_URL!;
 
-assert(process.env.DATABASE_URL);
-export const DATABASE_URL = process.env.DATABASE_URL;
+// assert(process.env.DATABASE_URL);
+export const DATABASE_URL = process.env.DATABASE_URL!;
 
-assert(process.env.RAGIE_API_BASE_URL);
-export const RAGIE_API_BASE_URL = process.env.RAGIE_API_BASE_URL;
+// assert(process.env.RAGIE_API_BASE_URL);
+export const RAGIE_API_BASE_URL = process.env.RAGIE_API_BASE_URL!;
 
-assert(process.env.RAGIE_API_KEY);
-export const RAGIE_API_KEY = process.env.RAGIE_API_KEY;
+// assert(process.env.RAGIE_API_KEY);
+export const RAGIE_API_KEY = process.env.RAGIE_API_KEY!;
 
-assert(process.env.RAGIE_WEBHOOK_SECRET);
-export const RAGIE_WEBHOOK_SECRET = process.env.RAGIE_WEBHOOK_SECRET;
+// assert(process.env.RAGIE_WEBHOOK_SECRET);
+export const RAGIE_WEBHOOK_SECRET = process.env.RAGIE_WEBHOOK_SECRET!;
