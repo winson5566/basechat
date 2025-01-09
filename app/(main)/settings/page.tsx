46071c30ee7768daa34f1c3d63f55ccb45a916fa
Bob Remeika <bob@ragie.ai>
2025-01-08 20:10:50 -0800

    Style general settings per the design

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -10,9 +10,42 @@ export default async function DataIndexPage() {
 
   return (
     <Main name={session.user.name} appLocation={AppLocation.SETTINGS}>
-      <div className="max-w-[1140px] w-full p-4 flex-grow flex flex-col">
-        <div className="flex w-full justify-between items-center pt-2">
-          <h1 className="font-bold text-[32px]">Settings</h1>
+      <div className="max-w-[1140px] w-full p-4 flex-grow flex">
+        <div className="w-[233px] flex flex-col pr-16">
+          <div className="bg-[#F5F5F7] px-3 py-2 rounded-lg font-semibold">General</div>
+          <div className="px-3 py-2">Users</div>
+        </div>
+        <div className="w-full p-4 flex-grow flex flex-col">
+          <div className="flex w-full justify-between items-center pt-2">
+            <h1 className="font-bold text-[32px]">Settings</h1>
+          </div>
+          <div className="mt-16">
+            <h3 className="font-semibold text-[16px]">Add sample questions to help your users get started</h3>
+            <div className="flex flex-col mt-8">
+              <label className="font-semibold text-[16px] mb-3">Question 1</label>
+              <input
+                type="text"
+                className="rounded-[8px] border border-[#D7D7D7] p-4 placeholder-[#74747A] text-[16px]"
+                placeholder="Type something"
+              />
+            </div>
+            <div className="flex flex-col mt-8">
+              <label className="font-semibold text-[16px] mb-3">Question 2</label>
+              <input
+                type="text"
+                className="rounded-[8px] border border-[#D7D7D7] p-4 placeholder-[#74747A] text-[16px]"
+                placeholder="Type something"
+              />
+            </div>
+            <div className="flex flex-col mt-8">
+              <label className="font-semibold text-[16px] mb-3">Question 3</label>
+              <input
+                type="text"
+                className="rounded-[8px] border border-[#D7D7D7] p-4 placeholder-[#74747A] text-[16px]"
+                placeholder="Type something"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </Main>
