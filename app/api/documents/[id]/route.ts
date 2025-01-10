96e3d9d7afae55b8479cf620c23bf5ccb003600b
Bob Remeika <bob@ragie.ai>
2025-01-10 07:32:21 -0800

    Enforce partition scope when retrieving document and summary

--- app/api/documents/[id]/route.ts
+++ app/api/documents/[id]/route.ts
@@ -1,12 +1,17 @@
 import { NextRequest } from "next/server";
 
 import { getRagieClient } from "@/lib/ragie";
+import { requireSession } from "@/lib/server-utils";
+import { getTenantByUserId } from "@/lib/service";
 
 export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const session = await requireSession();
+  const tenant = await getTenantByUserId(session.user.id);
+
   const { id } = await params;
 
-  // FIXME: This is not currently scoped to a tenant
-  const document = await getRagieClient().documents.get({ documentId: id });
-  const summary = await getRagieClient().documents.getSummary({ documentId: id });
+  const document = await getRagieClient().documents.get({ partition: tenant.id, documentId: id });
+  const summary = await getRagieClient().documents.getSummary({ partition: tenant.id, documentId: id });
+
   return Response.json({ ...document, summary: summary.summary });
 }
