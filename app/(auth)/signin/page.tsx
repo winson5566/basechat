84ba93c76b4e238bc44736af9c7a4d58a835ac6d
Bob Remeika <bob@ragie.ai>
2025-01-21 08:01:08 -0800

    Ensure invite flow works correctly with new credentials signup

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import * as settings from "@/lib/settings";
+
 import GoogleSignIn from "../google-sign-in";
 
 import SignIn from "./sign-in";
@@ -10,6 +12,11 @@ export default async function SignInPage({
   searchParams: Promise<{ registered?: number; redirectTo?: string }>;
 }) {
   const { registered, redirectTo } = await searchParams;
+  const signUpUrl = new URL("/signup", settings.BASE_URL);
+  if (redirectTo) {
+    signUpUrl.searchParams.set("redirectTo", redirectTo);
+  }
+
   return (
     <>
       <div className="self-start mb-12 text-[30px] font-bold">
@@ -33,7 +40,7 @@ export default async function SignInPage({
 
       <div className="mt-6 text-[16px]">
         <span className="text-[#74747A]">Need to create a new organization?&nbsp;</span>
-        <Link href="/signup" className="text-[#1D1D1F] hover:underline">
+        <Link href={signUpUrl.toString()} className="text-[#1D1D1F] hover:underline">
           Sign up
         </Link>
       </div>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -9,6 +9,11 @@ import SignUp from "./sign-up";
 
 export default async function SignUpPage({ searchParams }: { searchParams: Promise<{ redirectTo?: string }> }) {
   const { redirectTo } = await searchParams;
+  const signInUrl = new URL("/signin", settings.BASE_URL);
+  if (redirectTo) {
+    signInUrl.searchParams.set("redirectTo", redirectTo);
+  }
+
   return (
     <>
       <div className="self-start mb-12 text-[30px] font-bold">
@@ -29,7 +34,7 @@ export default async function SignUpPage({ searchParams }: { searchParams: Promi
 
       <div className="mt-6 text-[16px]">
         <span className="text-[#74747A]">Already using {settings.APP_NAME}?&nbsp;</span>
-        <Link href="/signin" className="text-[#1D1D1F] hover:underline">
+        <Link href={signInUrl.toString()} className="text-[#1D1D1F] hover:underline">
           Sign in
         </Link>
       </div>
