9b717f4095115ef8bdee9d63dde8116426d76726
Bob Remeika <bob@ragie.ai>
2025-02-07 00:34:52 -0800

    Hide nav for user role

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 
 export default async function AppLayout({ children }: Props) {
-  const { tenant } = await authOrRedirect();
+  const { tenant, profile } = await authOrRedirect();
 
   return (
     <div className="h-full w-full flex flex-col items-center bg-white">
@@ -18,7 +18,9 @@ export default async function AppLayout({ children }: Props) {
       <div className="h-full w-full flex-1 flex justify-center overflow-auto">
         <div className="h-full w-full flex flex-col items-center justify-center min-w-[500px]">{children}</div>
       </div>
-      <Footer className="h-[80px] shrink-0 w-full bg-[#27272A] flex items-center justify-center" />
+      {profile.role != "user" && (
+        <Footer className="h-[80px] shrink-0 w-full bg-[#27272A] flex items-center justify-center" />
+      )}
     </div>
   );
 }
