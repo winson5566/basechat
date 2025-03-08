9efabc5cc3c028dbbf438a3deb7c692518b446a1
Andrey Melnikov <vafilor@gmail.com>
2025-03-07 17:42:32 -0800

    Add loading prop to Button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,6 @@
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import { LoaderCircle } from "lucide-react";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
@@ -34,12 +35,17 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, children, asChild = false, loading, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    return (
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+        {loading ? <LoaderCircle className="h-5 w-5 animate-spin" /> : children}
+      </Comp>
+    );
   },
 );
 Button.displayName = "Button";
