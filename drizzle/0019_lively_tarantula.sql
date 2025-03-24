aeec4a135dee1382b8199fccbd7537a1ba4c6d2f
Bob Remeika <bob@ragie.ai>
2025-03-23 19:07:41 -0700

    Add backfill

--- drizzle/0019_lively_tarantula.sql
+++ drizzle/0019_lively_tarantula.sql
@@ -1,2 +1,3 @@
 ALTER TABLE "tenants" ADD COLUMN "slug" text;--> statement-breakpoint
 ALTER TABLE "tenants" ADD CONSTRAINT "tenants_slug_unique" UNIQUE("slug");
+UPDATE "tenants" SET "slug" = "id";
