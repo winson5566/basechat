fb8813730f59c70efd510211535657867d598272
Bob Remeika <bob@ragie.ai>
2025-03-25 08:20:51 -0700

    Add powered by footer for public chat bots

--- app/(main)/o/[slug]/layout.tsx
+++ app/(main)/o/[slug]/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 
+import RagieLogo from "@/components/ragie-logo";
 import { authOrRedirect } from "@/lib/server/utils";
 
 import Footer from "./footer";
@@ -22,6 +23,16 @@ export default async function MainLayout({ children, params }: Props) {
       </div>
       {profile.role == "admin" && (
         <Footer tenant={tenant} className="h-[80px] shrink-0 w-full bg-[#27272A] flex items-center justify-center" />
+      )}
+      {profile.role == "guest" && (
+        <div className="h-20 w-full bg-[#27272A] flex items-center justify-center">
+          <div className={`mr-2.5 text-md text-[#FEFEFE]`}>Powered by</div>
+          <div>
+            <a href="https://ragie.ai/?utm_source=oss-chatbot">
+              <RagieLogo />
+            </a>
+          </div>
+        </div>
       )}
     </div>
   );
