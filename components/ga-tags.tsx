9097e28589689a383ba50f3a9a699e6240e21fe1
Matt Kauffman <matt@ragie.ai>
2025-03-26 15:27:30 -0400

    Add optional google analytics support

---
+++ components/ga-tags.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Script from "next/script";
+
+export const GaTags = ({ gaKey }: { gaKey?: string }) => {
+  if (!gaKey) return null;
+  return (
+    <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaKey}`} strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${gaKey}');
+        `}
+      </Script>
+      <strong>OH HAI</strong>
+    </>
+  );
+};
