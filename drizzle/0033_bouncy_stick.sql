4601c233b635c3c2886e5433b30d020e5ba3518c
Bob Remeika <bob@ragie.ai>
2025-03-31 16:50:55 -0700

    Fix typo

--- drizzle/0033_bouncy_stick.sql
+++ drizzle/0033_bouncy_stick.sql
@@ -1,4 +1,4 @@
-b/ALTER TABLE "accounts" ADD COLUMN _access_token_expires_at timestamp;
+ALTER TABLE "accounts" ADD COLUMN _access_token_expires_at timestamp;
 UPDATE "accounts" SET _access_token_expires_at = TO_TIMESTAMP(access_token_expires_at);
 ALTER TABLE "accounts" DROP COLUMN access_token_expires_at;
 ALTER TABLE "accounts" RENAME COLUMN _access_token_expires_at TO access_token_expires_at;
