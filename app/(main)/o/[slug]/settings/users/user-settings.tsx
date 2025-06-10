f53ec29db4ad4219d78bae2a1bbb8a6bd0f9097f
Stone Werner <stone@ragie.ai>
2025-06-10 15:39:37 -0500

    fix user settings alignment

--- app/(main)/o/[slug]/settings/users/user-settings.tsx
+++ app/(main)/o/[slug]/settings/users/user-settings.tsx
@@ -270,7 +270,7 @@ export default function UserSettings({
 
   return (
     <div className="w-full p-4 flex-grow flex flex-col">
-      <div className="flex w-full justify-between items-center pt-2">
+      <div className="flex w-full justify-between items-center mb-16">
         <h1 className="font-bold text-[32px] text-[#343A40]">Users</h1>
         <div className="flex">
           <div className="flex flex-col justify-end">
